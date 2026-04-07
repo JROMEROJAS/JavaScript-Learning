34836c35f43ad93123ab344ef0709800d34eec45
JROMEROJAS <jacobromerojas@gmail.com>
2026-04-06 23:58:04 -0500

    Utilizacion de variables en JS

---
+++ Ejercicios-Implementacion/2.Variables/variables.js
@@ -0,0 +1,7 @@
+//Cuando querramos definir una variable en JS utilizaremos LET, seguido del nombre de la variable mas el valor que le vamos a asignar a dicha variable.
+
+//En JS para hacer uso de las variables debemos tener en cuenta unas "reglas" como por ejemplo el nombre de la variable debe comenzar con letras o guion bajos. Tampoco es posible utilizar palabras reservadas como el propio LET, ya que automaticamente arrojara error en el sistema.
+
+let nombre = "Hola Mundo";
+
+console.log(nombre);
