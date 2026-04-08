4d18fab9c6e640ba1024ad478c2359bc9867e854
JROMEROJAS <jacobromerojas@gmail.com>
2026-04-07 23:27:25 -0500

    Diferentes tipos de datos

---
+++ Ejercicios-Implementacion/3.Tipos de Datos/tiposDatos.js
@@ -0,0 +1,9 @@
+//Los tipos de datos en JavaScript principalmente se dividen en dos, primitivos y de referencia. En cuanto a los primitivos entran los siguientes:Number, String, Boolean, Undefined, Null. En cuanto a los de referencia entrarian: Arrays, Object, Functions, Clases.
+
+//Primeramente empecemos con los primitivos: 
+let numero = 1; //Number 
+let texto = "Hola Mundo"; //String
+let verdadero = true; //Boolean
+let falso = false; //Boolean
+let noDefinido; //Undefined
+let nulo = null; //Nulll
