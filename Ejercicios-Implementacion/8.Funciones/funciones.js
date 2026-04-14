0a6b5623ece06f61a05354d2d474c22c53b02267
JROMEROJAS <jacobromerojas@gmail.com>
2026-04-13 23:11:56 -0500

    Funciones

---
+++ Ejercicios-Implementacion/8.Funciones/funciones.js
@@ -0,0 +1,16 @@
+//Una funcion es un bloque de codigo reutilizable que se ejecuta cuando se llama. En otras palabras, es una forma de encapsular instrucciones para usarlas cuando se necesiten.
+
+//Algunas caracteristicas que tienen las funciones es que se pueden reutilizar, es decir, evitan a repetir codigo innecesario. Por otra parte, pueden recibir parametros y pueden retornar valores.
+
+function saludar() {
+    console.log('Hola mundo');
+}
+
+saludar();
+
+function suma() {
+    return 2+2;
+}
+
+let resultado = suma();
+console.log(resultado);
